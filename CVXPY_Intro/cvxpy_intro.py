2afb3123336d9adb9f89c199e2b81bc7cdac4272
Sarah <beethes@oregonstate.edu>
2022-05-27 12:02:26 -0700

    finish lab 8

--- CVXPY_Intro/cvxpy_intro.py
+++ CVXPY_Intro/cvxpy_intro.py
@@ -1,10 +1,12 @@
 # cvxpy_intro.py
 """Volume 2: Intro to CVXPY.
-<Name>
-<Class>
-<Date>
+Sarah Beethe
+MTH520
+05/27/22
 """
-
+#%%
+import cvxpy as cp
+import numpy as np
 
 def prob1():
     """Solve the following convex optimization problem:
@@ -21,9 +23,27 @@ def prob1():
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    x = cp.Variable(3, nonneg = True)
+    c = np.array([2, 1, 3])
+    objective = cp.Minimize(c.T @ x)
+    
+    A = np.array([1, 2, 0])
+    G = np.array([0, 1, -4])
+    P = np.array([2, 10, 3])
+    S = np.eye(3)
+    constraints = [A @ x <= 3, G @ x <= 1, P @ x >= 12, S @ x >= 0]
+    
+    problem = cp.Problem(objective, constraints)
+    opt_val = problem.solve()
+    opt_x = x.value
+    print(opt_x)
+    print(opt_val)
+    
+    return opt_x, opt_val
+prob1()
+    
 
-
+#%%
 # Problem 2
 def l1Min(A, b):
     """Calculate the solution to the optimization problem
@@ -39,9 +59,26 @@ def l1Min(A, b):
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 2 Incomplete")
 
+    
+    x = cp.Variable(len(A.T), nonneg = True)
+    objective = cp.Minimize(cp.norm(x, 1))
+    
+    constraints = [A @ x == b]
+    
+    problem = cp.Problem(objective, constraints)
+    opt_val = problem.solve()
+    opt_x = x.value
+    print(opt_x)
+    print(opt_val)
+    
+    return opt_x, opt_val
 
+A = np.array([[1, 2, 1, 1], [0, 3, -2, -1]])
+b = np.array([7, 4]).T
+l1Min(A, b)
+    
+#%%
 # Problem 3
 def prob3():
     """Solve the transportation problem by converting the last equality constraint
@@ -51,9 +88,30 @@ def prob3():
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    
+    A = np.array([[1, 1, 0, 0, 0, 0], 
+                  [0, 0, 1, 1, 0, 0], 
+                  [0, 0, 0, 0, 1, 1], 
+                  [-1, 0, -1, 0, -1, 0],
+                  [0, -1, 0, -1, 0, -1]])
+    b = np.array([7, 2, 4, -5, -8]).T
+    
+    x = cp.Variable(len(A.T), nonneg = True)
+    objective = cp.Minimize(cp.norm(x, 1))
+    
+    constraints = [A @ x == b]
+    
+    problem = cp.Problem(objective, constraints)
+    opt_val = problem.solve()
+    opt_x = x.value
+    print(opt_x)
+    print(opt_val)
+    
+    return opt_x, opt_val
 
+prob3()
 
+#%%
 # Problem 4
 def prob4():
     """Find the minimizer and minimum of
@@ -64,9 +122,22 @@ def prob4():
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    Q = np.array([[3, 2, 1],
+                  [2, 4, 2],
+                  [1, 2, 3]])
+    r = np.array([3, 0, 1])
+    x = cp.Variable(3)
+   
+    problem = cp.Problem(cp.Minimize(.5 * cp.quad_form(x, Q) + r.T @ x))
+    opt_val = problem.solve()
+    opt_x = x.value
+    print(opt_x)
+    print(opt_val)
+    
+    return opt_x, opt_val
 
-
+prob4()
+#%%
 # Problem 5
 def prob5(A, b):
     """Calculate the solution to the optimization problem
@@ -81,19 +152,20 @@ def prob5(A, b):
         The optimizer x (ndarray)
         The optimal value (float)
     """
-    raise NotImplementedError("Problem 5 Incomplete")
-
-
-# Problem 6
-def prob6():
-    """Solve the college student food problem. Read the data in the file 
-    food.npy to create a convex optimization problem. The first column is 
-    the price, second is the number of servings, and the rest contain
-    nutritional information. Use cvxpy to find the minimizer and primal 
-    objective.
+    x = cp.Variable(len(A.T), nonneg = True)
+    objective = cp.Minimize(cp.norm(A @ x - b, 2))
     
-    Returns (in order):
-        The optimizer x (ndarray)
-        The optimal value (float)
-    """	 
-    raise NotImplementedError("Problem 6 Incomplete")
+    constraints = [cp.sum(x) == 1]
+    problem = cp.Problem(objective, constraints)
+    opt_val = problem.solve()
+    opt_x = x.value
+    print(opt_x)
+    print(opt_val)
+    
+    return opt_x, opt_val
+
+A = np.array([[1, 2, 1, 1], [0, 3, -2, -1]])
+b = np.array([7, 4]).T
+
+prob5(A, b)
+
